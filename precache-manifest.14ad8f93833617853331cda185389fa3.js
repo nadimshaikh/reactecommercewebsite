bdb88ea3fbfa6fdce4e01be0ba96d2df97322664
nadimshaikh <nadshaikh7070@gmail.com>
2020-10-05 04:07:33 -0700

    Updates

---
+++ precache-manifest.14ad8f93833617853331cda185389fa3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9dbe31773c0465cae2460e09d4f682a",
+    "url": "/reactecommercewebsite/index.html"
+  },
+  {
+    "revision": "bb0c52f0b2b174a9aed5",
+    "url": "/reactecommercewebsite/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "e56cf9b94f8cce65a081",
+    "url": "/reactecommercewebsite/static/css/main.d6558245.chunk.css"
+  },
+  {
+    "revision": "bb0c52f0b2b174a9aed5",
+    "url": "/reactecommercewebsite/static/js/2.bcadd105.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactecommercewebsite/static/js/2.bcadd105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56cf9b94f8cce65a081",
+    "url": "/reactecommercewebsite/static/js/main.e66efb99.chunk.js"
+  },
+  {
+    "revision": "32c57261c22539b4b86a",
+    "url": "/reactecommercewebsite/static/js/runtime-main.901f07bf.js"
+  },
+  {
+    "revision": "61d1812a668957926326ab6a5d6669d6",
+    "url": "/reactecommercewebsite/static/media/Adidas.61d1812a.jpg"
+  },
+  {
+    "revision": "d498cdbab6c15c93ef9fbbc30f1655ae",
+    "url": "/reactecommercewebsite/static/media/Cr.d498cdba.jpg"
+  },
+  {
+    "revision": "4cdbe80bd611b13a51697a12361a104a",
+    "url": "/reactecommercewebsite/static/media/Hrx.4cdbe80b.jpg"
+  },
+  {
+    "revision": "c23751483a5d214ebe7f47fdd4aaf740",
+    "url": "/reactecommercewebsite/static/media/Hrxx.c2375148.jpg"
+  },
+  {
+    "revision": "0047954462211a6f82b83ed6313fc140",
+    "url": "/reactecommercewebsite/static/media/Nike.00479544.jpg"
+  },
+  {
+    "revision": "46cc472cc8152d44fb06b3eb60c9613f",
+    "url": "/reactecommercewebsite/static/media/Nikee.46cc472c.jpg"
+  },
+  {
+    "revision": "e5d89e5d78f686599e3d54274456e2bc",
+    "url": "/reactecommercewebsite/static/media/Puma.e5d89e5d.jpg"
+  },
+  {
+    "revision": "408089c11839089d00de1726471d9dd0",
+    "url": "/reactecommercewebsite/static/media/bg4.408089c1.jpg"
+  }
+]);
